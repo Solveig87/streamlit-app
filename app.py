3b4455a93c3dcb7c9aa8a35d01db6cb0d3ffd3e8
Solveig <solveig@LAPTOP-49D31SSK.localdomain>
2021-04-19 21:28:22 +0200

    correction

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def main():
         st.markdown("## Présentation du projet ")
         st.markdown("Cette application conçue avec Streamlit vous permettra de découvrir le domaine des hôtels écologiques et des langues peu dotée et, nous l'espérons, vous donnera envie d'investir dans ces domaines.")
         st.markdown("### À gauche dans le sidebar, vous trouverez 4 boutons à sélectionner pour aller à la page correspondante: ")
-        df_presentation = pd.DataFrame({'Thème': ['Présentation générale', 'NH Hotel : quelques graphiques','NH Hotel : Recherche d\'hôtels', ¨'Présentation du dictionnaire en ligne Ntealan'], 'Bouton': ['Présentation','NH Hotels : chiffres', 'NH Hotels : recherche', 'Ntealan']})
+        df_presentation = pd.DataFrame({'Thème': ['Présentation générale', 'NH Hotel : quelques graphiques','NH Hotel : Recherche d\'hôtels', 'Présentation du dictionnaire en ligne Ntealan'], 'Bouton': ['Présentation','NH Hotels : chiffres', 'NH Hotels : recherche', 'Ntealan']})
         st.dataframe(df_presentation)
         st.markdown("**NH Hotels : présentations**")
         st.markdown("Sur cette page, vous trouverez une présentation du domaine des hôtels écologiques et quelques graphiques effectués avec des données issues du site du groupe NH Hotels. L'objectif est de montrer que l'hôtellerie s'intéresse de plus en plus à l'écologie et que l'écologie n'empêche pas le confort !")
@@ -28,7 +28,7 @@ def main():
         st.markdown("***NH Hotels : Recherche***")
         st.markdown("Cette page vous permettra d'effectuer une recherche des hôtels écologiques du groupe NH Hotels par ville, afin de voir par vous-mêmes les jolies prestations de ces hôtels.")
         st.markdown("Les informations suivantes vous seront données pour chaque hôtel : nom, nombre d'étoiles, une photo, et un lien vers le site de NH Hotels pour plus d'informations.")
-        st.markdown("Afin de vous assurer des informations toujours à jour, notre moteur de recherche cherche en temps réel sur le site de NH HOtels, ce qui peut occasionner une certaine lenteur.")
+        st.markdown("Afin de vous assurer des informations toujours à jour, notre moteur de recherche cherche en temps réel sur le site de NH Hotels, ce qui peut occasionner une certaine lenteur.")
 
         st.markdown("**Ntealan : dictionnaire de langues africaines**")
         st.markdown("Vous pourrez consulter sur cette page quelques articles du dictionnaire en ligne de langues africaines peu dotées Ntealan.")

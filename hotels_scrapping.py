31a196c7cd36b341c0c6d3a2493e3285672eb40d
Solveig <solveig@LAPTOP-49D31SSK.localdomain>
2021-04-19 20:27:20 +0200

    modif

--- hotels_scrapping.py
+++ hotels_scrapping.py
@@ -2,40 +2,41 @@ from bs4 import BeautifulSoup
 from urllib.request import Request, urlopen
 import streamlit as st
 
-@st.cache(persist=True)
-def nh_scrap():
-    """Génère un dictionnaire donnant la liste des pages des hôtels par ville"""
+def hotels_scrap(ville):
+    """Génère la liste des hôtels éco-friendly d'une ville (liste de dictionnaires)
+    Paramètres d'entrée : nom de la ville, liste des pages web des hôtels par ville (dictionnaire)
+    Infos données en sortie pour chaque hôtel : nom, url, photo, nombre d'étoiles"""
 
     urlpage = 'https://www.nh-hotels.fr/hotels'
     req = Request(urlpage, headers={'User-Agent': 'Mozilla/5.0'})
     page = urlopen(req).read()
     soup = BeautifulSoup(page, 'html.parser')
 
-    pages_countries = [country.find("a").get("href") for country in soup.find_all('h3', attrs={'class': 'h6'})] 
-        
-    hotels_url = {}
+    pages_countries = [country.find("a").get("href") for country in soup.find_all('h3', attrs={'class': 'h6'})]
+
+    hotels_url = []
 
     for urlpage in pages_countries:
-            req = Request(urlpage, headers={'User-Agent': 'Mozilla/5.0'})
-            page = urlopen(req).read()
-            soup = BeautifulSoup(page, 'html.parser')
-            
-            for city in soup.find_all('div', attrs={'class': 'grid-item'}):
-                city_name = city.find('h2', attrs={'class': 'h4'}).getText().split()[-1]
-                hotels_url[city_name.lower()] = ["https://www.nh-hotels.fr" + hotel_url.find("a").get("href") for hotel_url in city.find_all("li")]
+        if len(hotels_url) > 0:
+            break
+        req = Request(urlpage, headers={'User-Agent': 'Mozilla/5.0'})
+        page = urlopen(req).read()
+        soup = BeautifulSoup(page, 'html.parser')
+        
+        for city in soup.find_all('div', attrs={'class': 'grid-item'}):
+            city_name = city.find('h2', attrs={'class': 'h4'}).getText().split()[-1]
+            if city_name.lower() == ville.lower() :
+                hotels_url = ["https://www.nh-hotels.fr" + hotel_url.find("a").get("href") for hotel_url in city.find_all("li")]
+                break
 
-    return hotels_url
+    if len(hotels_url) == 0:
+        return []
 
-def hotels_scrap(ville, hotels_url):
-    """Génère la liste des hôtels éco-friendly d'une ville (liste de dictionnaires)
-    Paramètres d'entrée : nom de la ville en minuscules (string), liste des pages web des hôtels par ville (dictionnaire)
-    Infos données en sortie pour chaque hôtel : nom, url, photo, nombre d'étoiles"""
-
-    hotels = []
-    hotel = {}
+    else:
+        hotels = []
+        hotel = {}
     
-    try:
-        for url in hotels_url[ville]:
+        for url in hotels_url:
             req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
             page = urlopen(req).read()
             soup = BeautifulSoup(page, 'html.parser')
@@ -50,20 +51,16 @@ def hotels_scrap(ville, hotels_url):
                 hotels.append(hotel)
             hotel = {}
         return hotels
-        
-    except:
-        return hotels
+
     
 def main():
-
-    hotels_url = nh_scrap()
 
     st.title("NH Group : eco-friendly hotels")
     user_input = st.text_input("Dans quel ville souhaitez-vous séjourner ?")
 
     if user_input :
 
-        hotels = hotels_scrap(user_input.lower(), hotels_url)
+        hotels = hotels_scrap(user_input)
 
         nb_result = str(len(hotels))+" résultats" if len(hotels) > 1 else str(len(hotels))+" résultat"
         st.header(nb_result)

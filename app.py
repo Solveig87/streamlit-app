c1a6a2ad4806b3ce47955bfb76a483e9605ae77b
Solveig <solveig@LAPTOP-49D31SSK.localdomain>
2021-04-19 22:04:54 +0200

    correction

--- app.py
+++ app.py
@@ -6,10 +6,11 @@ def main():
 
     # Page d'accueil du projet
     st.title("Smart Investment")
-    st.header("Logements écologiques et langues peu dotées")
-    st.sidebar.header("Projet pour le cours de Techniques web - M2 TAL, Inalco ")
+    st.header("Pour vous permettre d'investir l'avenir")
+    st.subheader("Logements écologiques et langues peu dotées")
+    st.sidebar.header("Projet - Techniques web")
+    st.sidebar.subheader("M2 TAL - Inalco ")
     st.sidebar.info('Auteur : Solveig PODER')
-    st.info("Pour vous permettre d'investir l'avenir")
 
     # Radio selector sur sidebar pour aller aux pages différentes
     st.sidebar.header("Votre sélection :")
@@ -20,8 +21,7 @@ def main():
         st.markdown("## Présentation du projet ")
         st.markdown("Cette application conçue avec Streamlit vous permettra de découvrir le domaine des hôtels écologiques et des langues peu dotée et, nous l'espérons, vous donnera envie d'investir dans ces domaines.")
         st.markdown("### À gauche dans le sidebar, vous trouverez 4 boutons à sélectionner pour aller à la page correspondante: ")
-        df_presentation = pd.DataFrame({'Thème': ['Présentation générale', 'NH Hotel : quelques graphiques','NH Hotel : Recherche d\'hôtels', 'Présentation du dictionnaire en ligne Ntealan'], 'Bouton': ['Présentation','NH Hotels : chiffres', 'NH Hotels : recherche', 'Ntealan']})
-        st.dataframe(df_presentation)
+
         st.markdown("**NH Hotels : présentations**")
         st.markdown("Sur cette page, vous trouverez une présentation du domaine des hôtels écologiques et quelques graphiques effectués avec des données issues du site du groupe NH Hotels. L'objectif est de montrer que l'hôtellerie s'intéresse de plus en plus à l'écologie et que l'écologie n'empêche pas le confort !")
         
@@ -34,7 +34,6 @@ def main():
         st.markdown("Vous pourrez consulter sur cette page quelques articles du dictionnaire en ligne de langues africaines peu dotées Ntealan.")
         st.markdown("Nous espérons que le travail de qualité effectué par les bénévoles vous donnera envie d'investir !")
 
-        st.write(" <--- Veuillez sélectionner un bouton pour commencer !")
         
     if radio == "NH Hotels : recherche":
         hotels_scraping.recherche()
